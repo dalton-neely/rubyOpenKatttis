d6683d6927ba407c925200a655e34cf20f9ee5cf
dalton-neely <daltonjn1@gmail.com>
2018-05-23 17:22:53 -0600

    second try

--- source.rb
+++ source.rb
@@ -12,33 +12,114 @@ def getScores (players)
     return scores
 end
 
-def findIndexDown (scores)
-    lowest = scores[0]
-    lowestIndex = 0
-    scores.each_with_index do |score, index|
-        if lowest > scores[index]
-            lowest = scores[index]
-            lowestIndex = index
-        end
+def printOutput (output)
+    index = 0
+    while index < output.length
+        puts output[index]
+        index += 1
     end
-    return lowestIndex, scores.length - 1
 end
 
-def findIndexUp (scores)
-    highest = scores[0]
-    highestIndex = 0
-    scores.each_with_index do |score, index|
-        if highest < scores[index]
-            highest = scores[index]
-            highestIndex = index
+def locateI (players)
+    i = 0
+    upOrDown = nil
+    players.each_with_index do |player, index|
+        if (index != players.length - 1) && (index != 0)
+            prv = index - 1
+            nxt = index + 1
+            if (player < players[nxt] && player < players[prv]) || (player > players[nxt] && player > players[prv])
+                i = index
+                if (player > players[nxt] && player > players[prv])
+                    upOrDown = "up"
+                else
+                    upOrDown = "down"
+                end
+                break
+            end
         end
     end
-    return highestIndex, 0
+    return i, upOrDown
+end
+
+def locateJ (iVal, players, upOrDown)
+    j = 0
+    players.each_with_index do |player, index|
+        if upOrDown == "down"
+            if index = players.length - 1
+                if player > iVal
+                    j = players.length - 1
+                end
+            else
+                if player < iVal
+                    j = index
+                    break
+                end
+            end
+        else
+            if player > iVal
+                j = index
+                break
+            end
+        end
+    end
+    return j
+end
+
+def isSorted? (players)
+    players.each_with_index do |player, index|
+        if index != players.length - 1
+            if player < players[index + 1]
+                return false
+            end
+        end
+    end
+    return true
+end
+
+def moveUp(i,j,players)
+    temp = players[i]
+    while i != j do
+        i -= 1
+        players[i + 1] = players[i]
+    end
+    players[j] = temp
+    return players
+end
+
+def moveDown(i,j,players)
+    temp = players[i]
+    while i != j do
+        i += 1
+        players[i - 1] = players[i]
+    end
+    players[j] = temp
+    return players
+end
+
+def move(i,j,players)
+    if i > j
+        return moveUp(i,j,players)
+    else
+        return moveDown(i,j,players)
+    end
+end
+
+def ranklistSort (players)
+    output = Array.new()
+    numMoves = 0
+    until isSorted?(players)
+        i, upOrDown = locateI(players)
+        j = locateJ(players[i], players, upOrDown)
+        numMoves += 1
+        output.push("#{i + 1} #{j + 1}")
+        players = move(i, j, players)
+    end
+    output = ["#{numMoves}"] + output
+    return output
 end
 
 $numPlayers = STDIN.gets.to_i 
 $scores = getScores($numPlayers)
-$output = Array.new()
 
 if ARGV[0] <=> "test"
     
@@ -48,36 +129,37 @@ if ARGV[0] <=> "test"
             assert_equal([20, 30, 5, 15, 10], $scores)
         end
 
-        def test_findIndexDown
-            assert_equal([2, 4] , findIndexDown($scores))
+        def test_locateI
+            assert_equal([1, "up"], locateI($scores))
+            assert_equal([2, "down"], locateI([30,20,5,15,10]))
         end
 
-        def test_findIndexUp
-            assert_equal([1, 0], findIndexUp($scores))
+        def test_locateJ
+            assert_equal(0, locateJ(30, $scores, "up"))
+            assert_equal(4, locateJ(5, [30, 20, 5, 15, 10], "down"))
         end
-    
+
+        def test_isSorted
+            assert_equal(true, isSorted?([30, 20, 15, 10, 5]))
+            assert_equal(false, isSorted?([30, 20, 45, 10, 5]))
+            assert_equal(false, isSorted?([1, 20, 15, 10, 5]))
+            assert_equal(false, isSorted?([30, 20, 15, 10, 45]))
+        end
+
+        def test_moveUp
+            assert_equal([30,20,5,15,10], moveUp(1,0, $scores))
+        end
+
+        def test_moveDown
+            assert_equal([30,20,15,10,5], moveDown(2,4,[30,20,5,15,10]))
+        end
+
+        def test_ranklistSort
+            assert_equal(["2", "2 1", "3 5"], ranklistSort([20,30,5,15,10]))
+        end
     end
 
-else 
-    index = 0
-    i = 0
-    j = 0
-    state = $scores
-    offset = 1
-    while index < $numPlayers - 1 do 
-        i,j = findIndexUp(state)
-        if i != j
-            $output.push("#{i + offset} #{j + offset}")
-        end
-        offset += 1
-        state.delete_at(i)
-        index += 1
-        i,j = findIndexDown(state)
-        if i != j
-            $output.push("#{i + offset} #{j + offset}")
-        end
-        state.delete_at(i)
-        index += 1
-    end
-    print $output
+else
+    output = ranklistSort($scores)
+    printOutput(output)
 end
